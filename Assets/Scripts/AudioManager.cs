b6d8c59f1ad4590284067cb6a7381d80cb33eb66
Juan Borges <juan.vegaborges@gmail.com>
2019-07-17 20:26:59 -0400

    Add files via upload

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    //TODO: Use "instance" instead of "audioManager"
+    public static AudioManager audioManager;
+
+    [Header("AudioSource & SFX")]
+    [SerializeField]
+    private AudioSource _audioSource;
+
+
+    [SerializeField]
+    private AudioClip _uiTouchSFX;
+    [SerializeField]
+    private AudioClip _towerTouchedSFX;
+    [SerializeField]
+    private AudioClip _gameStartSFX;
+
+    [Header("Audio UI")]
+    [SerializeField]
+    private Image _audioImage;
+    [SerializeField]
+    private Sprite _soundsOnIcon;
+    [SerializeField]
+    private Sprite _soundsOffIcon;
+
+    private void Awake()
+    {
+        if (audioManager == null)
+            audioManager = GetComponent<AudioManager>();
+    }
+
+    public void PlayGameStart() => _audioSource.PlayOneShot(_gameStartSFX);
+
+    public void PlayTowerTouched() => _audioSource.PlayOneShot(_towerTouchedSFX);
+
+    public void PlayUiTouch() => _audioSource.PlayOneShot(_uiTouchSFX);
+
+    public void ChangeAudioListenerState()
+    {
+        AudioListener.pause = !AudioListener.pause;
+        UpdateAudioIcon(true);
+    }
+
+    public void UpdateAudioIcon(bool isButtonTouched)
+    {
+        if (AudioListener.pause == true)
+            _audioImage.overrideSprite = _soundsOffIcon;
+        else if (AudioListener.pause == false)
+        {
+            _audioImage.overrideSprite = _soundsOnIcon;
+
+            if (isButtonTouched)
+                PlayUiTouch();
+        }
+    }
+}
---
+++ Assets/Scripts/DiskRank.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskRank : MonoBehaviour
+{
+    public int rank = 3;
+}
